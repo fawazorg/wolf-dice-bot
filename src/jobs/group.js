975c545d10cbd057a4d1f051447aed7fa26230f3
fawazorg <foazorg@gmail.com>
2022-04-27 23:40:52 +0300

    fix update timer

--- src/jobs/group.js
+++ src/jobs/group.js
@@ -6,13 +6,15 @@
  */
 const UpdateTimer = async (api, data, game) => {
   let g = game.find(data.id);
-  if (g.players.length <= 1) {
-    return await game.finish(g);
-  } else {
-    if (g.joinable) {
-      return await game.start(g);
+  if (g) {
+    if (g.players.length <= 1) {
+      return await game.finish(g);
+    } else {
+      if (g.joinable) {
+        return await game.start(g);
+      }
+      return;
     }
-    return;
   }
   return;
 };
